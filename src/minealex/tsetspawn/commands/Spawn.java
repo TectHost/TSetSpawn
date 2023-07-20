7ff0affc545f86b7bced64606f6be0f4c5784ea2
Tect.host <tect.host@gmail.com>
2023-07-20 16:07:09 +0200

    Add files via upload

--- src/minealex/tsetspawn/commands/Spawn.java
+++ src/minealex/tsetspawn/commands/Spawn.java
@@ -2,7 +2,9 @@ package minealex.tsetspawn.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,20 +13,16 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Spawn implements CommandExecutor, Listener {
     private Plugin plugin;
     private Map<String, Long> cooldowns = new HashMap<>();
-    private Set<String> firstTimeTeleport = new HashSet<>();
     private boolean enableVoidTeleport;
     private String voidTeleportMessage;
     private FileConfiguration config;
@@ -34,13 +32,8 @@ public class Spawn implements CommandExecutor, Listener {
         this.config = plugin.getConfig();
         Bukkit.getPluginManager().registerEvents(this, plugin);
 
-        boolean enableFirstTimeTeleport = config.getBoolean("Config.enable-first-time-teleport", true);
         enableVoidTeleport = config.getBoolean("Config.enable-void-teleport", true);
         voidTeleportMessage = config.getString("Config.Translate.void-teleport-message", "&5TSetSpawn &e> &fTeleported to the Spawn");
-
-        if (!enableFirstTimeTeleport) {
-            firstTimeTeleport.clear();
-        }
     }
 
     public boolean onCommand(final CommandSender sender, Command command, String label, String[] args) {
@@ -59,14 +52,6 @@ public class Spawn implements CommandExecutor, Listener {
         if (!config.contains("Config.Spawn.x") || !config.contains("Config.Spawn.y") || !config.contains("Config.Spawn.z")) {
             String spawnNo = "Config.Translate.spawn-not-placed";
             jugador.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString(spawnNo)));
-            return true;
-        }
-
-        if (firstTimeTeleport.contains(jugador.getName())) {
-            jugador.teleport(getSpawnLocationFromConfig());
-            firstTimeTeleport.remove(jugador.getName());
-            String firstTimeTeleportMsg = "Config.Translate.first-time-teleport";
-            jugador.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString(firstTimeTeleportMsg)).replaceAll("%player%", jugador.getName()));
             return true;
         }
 
@@ -101,7 +86,6 @@ public class Spawn implements CommandExecutor, Listener {
         jugador.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString(countdownMessage)));
 
         new BukkitRunnable() {
-            @SuppressWarnings("deprecation")
             @Override
             public void run() {
                 boolean particlesEnabled = config.getBoolean("Config.Particles.enabled", true);
@@ -116,9 +100,12 @@ public class Spawn implements CommandExecutor, Listener {
                         double offsetX = Math.cos(angle) * radius;
                         double offsetZ = Math.sin(angle) * radius;
                         Location particleLocation = l.clone().add(offsetX, 0.5, offsetZ);
-                        jugador.playEffect(particleLocation, org.bukkit.Effect.valueOf(particleType), 0);
+                        jugador.getWorld().playEffect(particleLocation, Effect.valueOf(particleType), 0);
                     }
                 }
+
+                // Reproducir el sonido para el jugador justo antes de teletransportar
+                playTeleportSound(jugador);
 
                 jugador.teleport(l);
                 String onSpawn = "Config.Translate.spawn";
@@ -129,6 +116,19 @@ public class Spawn implements CommandExecutor, Listener {
         return true;
     }
 
+    private void playTeleportSound(Player player) {
+        // Reproducir el sonido si está habilitado en la configuración
+        boolean enableSound = config.getBoolean("Config.Sound.enable-sound", true);
+        if (enableSound) {
+            String soundTypeString = config.getString("Config.Sound.sound-type", "ENTITY_PLAYER_LEVEL_UP");
+            float volume = (float) config.getDouble("Config.Sound.volume", 1.0);
+            float pitch = (float) config.getDouble("Config.Sound.pitch", 1.0);
+
+            // Reproducir el sonido para el jugador
+            player.getWorld().playSound(player.getLocation(), Sound.valueOf(soundTypeString), volume, pitch);
+        }
+    }
+
     private Location getSpawnLocationFromConfig() {
         double x = Double.parseDouble(config.getString("Config.Spawn.x"));
         double y = Double.parseDouble(config.getString("Config.Spawn.y"));
@@ -137,12 +137,6 @@ public class Spawn implements CommandExecutor, Listener {
         float pitch = Float.parseFloat(config.getString("Config.Spawn.pitch"));
         World world = plugin.getServer().getWorld(config.getString("Config.Spawn.world"));
         return new Location(world, x, y, z, yaw, pitch);
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player jugador = event.getPlayer();
-        firstTimeTeleport.add(jugador.getName());
     }
 
     @EventHandler
