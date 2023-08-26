5c216bd507827362621d9021e4e0fa932b7036c4
Tect.host <tect.host@gmail.com>
2023-08-26 12:06:25 +0200

    Add files via upload

--- src/minealex/tsetspawn/TSetSpawn.java
+++ src/minealex/tsetspawn/TSetSpawn.java
@@ -17,46 +17,46 @@ import minealex.tsetspawn.events.Join;
 import minealex.tsetspawn.events.Welcome;
 import minealex.tsetspawn.events.Death;
 
-public class TSetSpawn extends JavaPlugin{
-	public String rutaConfig;
-	PluginDescriptionFile pdffile = getDescription();
-	public String version = pdffile.getVersion();
-	public String nombre = ChatColor.BLUE+"["+pdffile.getName()+ChatColor.BLUE+"]";
-	
-	public void onEnable() {
-		Bukkit.getConsoleSender().sendMessage(nombre+ChatColor.GREEN+" Has been activated (version: "+version+")");
-		registerCommands();
-		registerEvents();
-		registerConfig();
-		if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+public class TSetSpawn extends JavaPlugin {
+    public String rutaConfig;
+    PluginDescriptionFile pdffile = getDescription();
+    public String version = pdffile.getVersion();
+    public String nombre = ChatColor.BLUE + "[" + pdffile.getName() + ChatColor.BLUE + "]";
+
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(nombre + ChatColor.GREEN + " Has been activated (version: " + version + ")");
+        registerCommands();
+        registerEvents();
+        registerConfig();
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             new Placeholders(this).register();
         }
-	}
-	
-	public void onDisable() {
-		Bukkit.getConsoleSender().sendMessage(nombre+ChatColor.RED+" Has been deactivated (version: "+version+")");
-	}
-	
-	public void registerCommands() {
-		this.getCommand("tsetspawn").setExecutor(new Commands(this));
-		this.getCommand("spawn").setExecutor(new Spawn(this));
-		this.getCommand("setspawn").setExecutor(new SetSpawn(this));
-	}
-	
-	public void registerEvents() {
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(new Enter(this), this);
-		pm.registerEvents(new Join(this), this);
-		pm.registerEvents(new Welcome(this), this);
-		pm.registerEvents(new Death(this), this);
-	}
-	
-	public void registerConfig() {
-		File config = new File(this.getDataFolder(),"config.yml");
-		rutaConfig = config.getPath();
-		if(!config.exists()) {
-			this.getConfig().options().copyDefaults(true);
-			saveConfig();
-		}
-	}
+    }
+
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(nombre + ChatColor.RED + " Has been deactivated (version: " + version + ")");
+    }
+
+    public void registerCommands() {
+        this.getCommand("tsetspawn").setExecutor(new Commands(this));
+        this.getCommand("spawn").setExecutor(new Spawn(this));
+        this.getCommand("setspawn").setExecutor(new SetSpawn(this));
+    }
+
+    public void registerEvents() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new Enter(this), this);
+        pm.registerEvents(new Join(this), this);
+        pm.registerEvents(new Welcome(this), this);
+        pm.registerEvents(new Death(this), this);
+    }
+
+    public void registerConfig() {
+        File config = new File(this.getDataFolder(), "config.yml");
+        rutaConfig = config.getPath();
+        if (!config.exists()) {
+            this.getConfig().options().copyDefaults(true);
+            saveConfig();
+        }
+    }
 }
